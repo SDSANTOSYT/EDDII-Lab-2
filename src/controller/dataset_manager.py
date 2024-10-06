eb065dbb90e65e89afd41a571a21ddd3e18eb1b0
SDSANTOSYT <129241099+SDSANTOSYT@users.noreply.github.com>
2024-10-05 21:21:27 -0500

    Update dataset_manager.py
    se crearon mas funciones para ayudar en el manejo del dataset

--- src/controller/dataset_manager.py
+++ src/controller/dataset_manager.py
@@ -1,16 +1,28 @@
 import csv
 
 # función para obtener la lista de todos los aeropuertos que serán los nodos del grafo
-def aeroport_node_list() -> list :
-    aeroports = []
+def aeroport_node_list() -> dict :
+    aeroports = {}
     with open('src/flights_final.csv',"r",encoding="utf-8") as dataset:
         reader = csv.reader(dataset)
         next(reader)
-        for line in reader:
-            if line[0] not in aeroports:
-                aeroports.append(line[0])
+        aeroports = {line[0]: line[0:6] for line in reader}
+            
+            #if line[0] not in aeroports:
+             #   aeroports.append(line[0])
         return aeroports
 
+def aeroport_edges_list() -> list:
+    connections = []
+    with open('src/flights_final.csv',"r",encoding="utf-8") as dataset:
+        reader = csv.reader(dataset)
+        next(reader)
+        connections_1 = list(set([str(f"{line[0]},{line[6]}") for line in reader]))
+        connections = [connection for connection in connections_1 if str(f"{connection[4:]},{connection[0:3]}") not in connections ]
+        return connections
+
 aeropuertos = aeroport_node_list()
-print(aeropuertos)
+conecciones = aeroport_edges_list()
+#print(aeropuertos.keys())
 print(len(aeropuertos))
+print(len(conecciones))
