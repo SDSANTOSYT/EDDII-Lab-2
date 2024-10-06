ed20a5eb243a5a717b6a41a6058969d4ad95430f
SDSANTOSYT <129241099+SDSANTOSYT@users.noreply.github.com>
2024-10-06 00:48:13 -0500

    Update dataset_manager.py
    se mejoraron las funciones para el manejo del dataset, ahora se puede obtener la lista de adyacencia de los aeropuertos

--- src/controller/dataset_manager.py
+++ src/controller/dataset_manager.py
@@ -1,28 +1,25 @@
 import csv
 
 # función para obtener la lista de todos los aeropuertos que serán los nodos del grafo
-def aeroport_node_list() -> dict :
-    aeroports = {}
+def airport_node_list() -> dict :
+    airports = {}
     with open('src/flights_final.csv',"r",encoding="utf-8") as dataset:
         reader = csv.reader(dataset)
         next(reader)
-        aeroports = {line[0]: line[0:6] for line in reader}
-            
-            #if line[0] not in aeroports:
-             #   aeroports.append(line[0])
-        return aeroports
+        airports = {line[0]: line[0:6] for line in reader}
+        return airports
 
-def aeroport_edges_list() -> list:
-    connections = []
+def airports_adjacency_list() -> dict:
     with open('src/flights_final.csv',"r",encoding="utf-8") as dataset:
         reader = csv.reader(dataset)
         next(reader)
-        connections_1 = list(set([str(f"{line[0]},{line[6]}") for line in reader]))
-        connections = [connection for connection in connections_1 if str(f"{connection[4:]},{connection[0:3]}") not in connections ]
-        return connections
+        data = list(reader)
+        # esta variable es un diccionario que tiene como claves los aeropuertos y de valores una lista de los aeropuertos que son destinos para ese aeropuerto sin repetirse
+        airport_connections = {airport: list(set([connection[6] for connection in data if connection[0] == airport])) for airport in airport_node_list().keys()}        
+        return airport_connections
 
-aeropuertos = aeroport_node_list()
-conecciones = aeroport_edges_list()
-#print(aeropuertos.keys())
+aeropuertos = airport_node_list()
+conecciones = airports_adjacency_list()
+
 print(len(aeropuertos))
 print(len(conecciones))
