592f95bcefc0da4904e659675aa4591636da6143
ALEXYSALCED0 <alexysalcedo01@gmail.com>
2024-10-20 01:35:25 -0500

    Update view.py

--- src/view/view.py
+++ src/view/view.py
@@ -24,13 +24,32 @@ class App:
 
         root.mainloop()
 #Cuadros de las componentes  
-class GraphCompontensInfo(ctk.CTkFrame):
-    def __init__(self,nVertices,minimunExpansionTreeWeight,componentNumbe,master) -> None:
-        self.config(master=master,width=275,height=300)
+class GraphComponentsInfo(ctk.CTkFrame):
+    def __init__(self, nVertices, minimumExpansionTreeWeight, componentNumber, master=None, **kwargs):
+        # Llamada al constructor de la clase padre
+        super().__init__(master, width=275, height=300, fg_color='#f6b6a8',**kwargs)
+
+        componentNumberLbl=ctk.CTkLabel(master=self,text=componentNumber, font=('Nunito',30),text_color='#000000').place(relx=0.5, y=20, anchor='center')
+        nVerticesLbl=ctk.CTkLabel(master=self,text=nVertices, font=('Nunito',30),text_color='#000000').place(relx=0.5, y=60, anchor='center')
+        minimumExpansionTreeWeightLbl=ctk.CTkLabel(master=self, text=minimumExpansionTreeWeight, font=('Nunito',30),text_color='#000000').place(relx=0.5,y=100,anchor='center')
+class minimumPathsAirports(ctk.CTkFrame):
+    def __init__(self, airportCode, airportName, airportCity, airportCountry, airportLatitude,airportlongitude, master=None, **kwargs):
+        # Llamada al constructor de la clase padre
+        super().__init__(master, width=275, height=300, fg_color='#f6b6a8',**kwargs)
+        airportCodeLbl=ct
+        airportNameLbl=1
+        airportCityLbl=1
+        airportCountryLbl=1
+        airportLatitude=1
+        airportlongitude=1
 
 
 
 
-
-
-DebugApp=App()
+#prueba del frame 
+ruta= ctk.CTk()
+ruta.geometry("500x500")
+ruta.title("Prueba")
+cuadro=GraphComponentsInfo(nVertices=1,minimumExpansionTreeWeight=2,componentNumber=3,master=ruta)
+cuadro.place(x=10,y=10)
+ruta.mainloop()
